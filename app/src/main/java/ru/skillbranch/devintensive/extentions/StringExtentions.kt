738d5d709be66ab3d53c0e54161bd8a22385bac5
soufee <soufee@mail.ru>
2019-12-05 21:12:36 +0300

    Рефакторинг 3            Убрал stripHtml

--- app/src/main/java/ru/skillbranch/devintensive/extentions/StringExtentions.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extentions/StringExtentions.kt
@@ -5,8 +5,8 @@ fun String.truncate(length: Int = 16): String{
     return if (trimmedMes.length <= length) trimmedMes else trimmedMes.substring(0, length).trim() + "..."
 }
 
-fun String.stripHtml(): String{
-    val htmlRegex = Regex("(<.*?>)|(&[^ а-я]{1,4}?;)")
-    val spaceRegex = Regex(" {2,}")
-    return this.replace(htmlRegex, "").replace(spaceRegex, " ")
-}
+//fun String.stripHtml(): String{
+//    val htmlRegex = Regex("(<.*?>)|(&[^ а-я]{1,4}?;)")
+//    val spaceRegex = Regex(" {2,}")
+//    return this.replace(htmlRegex, "").replace(spaceRegex, " ")
+//}
